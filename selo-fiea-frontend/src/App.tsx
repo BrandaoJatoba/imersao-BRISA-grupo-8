923ff83abd347c8cfa183ab2a545cd7ada677b97
Alexandre Barbosa <xnd.dev@gmail.com>
2025-10-15 18:29:02 -0300

    feat: create the administrator registration page

--- selo-fiea-frontend/src/App.tsx
+++ selo-fiea-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import { LoginPage } from './pages/LoginPage';
 import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
 import { ResetPasswordPage } from './pages/ResetPasswordPage';
+import { RegistrationPage } from './pages/AdminRegistrationPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/admin-register" element={<RegistrationPage />} />
     </Routes>
   )
 }
