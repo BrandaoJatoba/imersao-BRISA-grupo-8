9a177bfea1b3117170ec13c3ebd49ec81eb32493
luizwhirl <leeshupativani@gmail.com>
2025-10-25 21:31:16 -0300

    audit page

--- selo-fiea-frontend/src/App.tsx
+++ selo-fiea-frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import  LandingPage  from './pages/LandingPage';
+import  LandingPage  from './pages/LandingPage';
 import { LoginPage } from './pages/LoginPage';
 import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
 import { ResetPasswordPage } from './pages/ResetPasswordPage';
@@ -8,24 +8,27 @@ import { ManagerRegistrationPage } from './pages/ManagerRegistrationPage';
 import { DashboardPage } from './pages/DashboardPage';
 import { ProfilesPage } from './pages/ProfilesPage';
 import { BadgesPage } from './pages/BadgesPage';
+import { AuditsPage } from './pages/AuditsPage';
 
 function App() {
-  return (
-    <Routes>
-      {/* Rotas Públicas */}
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-      <Route path="/reset-password" element={<ResetPasswordPage />} />
-      <Route path="/admin-register" element={<AdminRegistrationPage />} />
-      <Route path="register" element={<ManagerRegistrationPage />} />
+  return (
+    <Routes>
+      {/* Rotas Públicas */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/admin-register" element={<AdminRegistrationPage />} />
+      <Route path="register" element={<ManagerRegistrationPage />} />
 
-      {/* Rotas Protegidas (após login) */}
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/dashboard/perfis" element={<ProfilesPage />} />
-      <Route path="/dashboard/selos" element={<BadgesPage />} />
-    </Routes>
-  )
+      {/* Rotas Protegidas (após login) */}
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/dashboard/perfis" element={<ProfilesPage />} />
+      <Route path="/dashboard/selos" element={<BadgesPage />} />
+      {/* Rota Adicionada */}
+      <Route path="/dashboard/auditorias" element={<AuditsPage />} />
+    </Routes>
+  )
 }
 
 export default App;
